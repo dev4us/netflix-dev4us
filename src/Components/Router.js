aed8d2e85c8d85e7056b38fc0e61ff09a930eac4
dev4us <bronxofsoul@gmail.com>
2019-02-28 23:17:17 +0900

    design header and set router

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -5,17 +5,21 @@ import {
   Redirect,
   Switch
 } from "react-router-dom";
+import Header from "Components/Header";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
 export default () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" component={TV} />
-      <Route path="/search" component={Search} />
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" component={TV} />
+        <Route path="/search" component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </Router>
 );
